4bdf62b3dd2173f39a3ee01fe37d2a1ce6048b3c
error 401 <1225tandin@gmail.com>
2021-12-17 10:45:43 +0600

    name changed

--- tandin_hash/app.py
+++ tandin_hash/app.py
@@ -1,17 +1,22 @@
-
-
+#
 # https://docs.python.org/3/library/hashlib.html
 import hashlib
+from os import read
 
 def hash_sha3_256(msg):
-  # Task1. Implement function using hashlib
-  str1 = 'hello from Bhutan'
-  
-  return ''
+  hash_obj = hashlib.sha3_256( ) # Call the hash funcstion in this case sha3_256
+  hash_obj.update(msg) # Update the hash function with msg parameter passed.
+  return hash_obj.hexdigest()
 
 def hash_sha3_256_file(path): 
   # Task2. Implement function using hashlib, open func
-  return ''
+  h = hashlib.sha3_256()
+  with open(path,'rb') as file: # read file in binary mode r-read, b-binary
+    fread = 0
+    while fread != b'':   # read till the end of the binary file.
+          fread=file.read()
+          h.update(fread)
+  return h.hexdigest()
 
 if __name__ == '__main__':
   print(hash_sha3_256(b'hello from Bhutan')) # return '277e3e07d56684ec98cc2796b6f2127d36742d1de2c19deeba7ec4ebe04f788e'
